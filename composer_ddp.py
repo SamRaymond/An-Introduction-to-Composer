b94f9e79f006af8fb66b72581c8ce94226910b7d
Sam Raymond <sjr@mit.edu>
2024-09-12 15:28:57 -0400

    Update composer_ddp.py

--- composer_ddp.py
+++ composer_ddp.py
@@ -56,8 +56,8 @@ transform = transforms.Compose([
 
 # Load CIFAR-10 dataset
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./my_data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./my_data', train=False, download=True, transform=transform)
 dist.barrier()
 # Create distributed samplers
 train_sampler = dist.get_sampler(trainset, shuffle=True)

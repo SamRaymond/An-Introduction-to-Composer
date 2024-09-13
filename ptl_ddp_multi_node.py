39d3bf63ba2e86f72d47844fd3ab8fecddfb8031
SamRaymond <sjr@mit.edu>
2024-09-13 18:50:43 -0400

    4 nodes

--- ptl_ddp_multi_node.py
+++ ptl_ddp_multi_node.py
@@ -108,7 +108,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 trainer = L.Trainer(
     max_epochs=num_epochs,
     devices=8, #how many GPUs to use per node
-    num_nodes=2,
+    num_nodes=4,
     accelerator="gpu",
     )
 trainer.fit(model=model, train_dataloaders=trainloader)
